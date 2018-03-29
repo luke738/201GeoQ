9e0957f5ba4eb1f0fef8fd8fe440ad24383e3ddb
Luke St. Regis <stregis@usc.edu>
2018-03-28 23:51:54 -0700

    Leaderboard is functional but hideous

--- src/Leaderboard.java
+++ src/Leaderboard.java
@@ -1,9 +1,15 @@
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 @WebServlet(name = "Leaderboard", urlPatterns = "/Leaderboard")
 public class Leaderboard extends HttpServlet
@@ -14,7 +20,29 @@ public class Leaderboard extends HttpServlet
         //Either check the gameid explicitly or move directly to getting the data for the leaderboard from the game
         //logic. Not entirely sure how that will work, so this is where I'm going to hardcode the data in.
         ////////DEV CODE ONLY////////
-
+        List<LeaderboardDataElement> boardData = new ArrayList<>();
+        boardData.add(new LeaderboardDataElement("ttrojan", "15", 1234));
+        boardData.add(new LeaderboardDataElement("jmiller", "45", 456));
+        boardData.add(new LeaderboardDataElement("stregis", "80", 125437834));
+        boardData.add(new LeaderboardDataElement("orahimi", "55", 12453434));
+        boardData.add(new LeaderboardDataElement("manipusg", "40", 12543434));
+        boardData.add(new LeaderboardDataElement("nathanzh", "60", 1000000));
+        boardData.add(new LeaderboardDataElement("kaagarwa", "60", 100000));
         ////////END DEV. CODE////////
+        boardData.sort(Comparator.comparingInt((LeaderboardDataElement o) -> -Integer.valueOf(o.score)).thenComparingInt(o -> o.timeTaken));
+        //Loop to see if any ties happened and add emoji to indicate winner/loser to user
+        for(int i = 0; i < boardData.size()-1; i++)
+        {
+            if(boardData.get(i).score.equals(boardData.get(i+1).score))
+            {
+                boardData.get(i).score += "&#x23F3";
+                boardData.get(i+1).score += "&#x231B";
+            }
+        }
+        Gson gson = new Gson();
+        String outData = gson.toJson(boardData);
+        PrintWriter pw = response.getWriter();
+        pw.println(outData);
+        pw.flush();
     }
 }
--- web/js/leaderboard.js
+++ web/js/leaderboard.js
@@ -1,14 +1,14 @@
 function loadLeaderboard()
 {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "/Leaderboard?gameid=0", false) //Not sure on the gameid system, gonna hardcode it for now
+    xhttp.open("GET", "/Leaderboard?gameid=0", false); //Not sure on the gameid system, gonna hardcode it for now
     xhttp.send();
     var results = JSON.parse(xhttp.response);
     var board = document.getElementById("board");
     for(var i = 0; i<results.length; i++)
     {
         board.innerHTML += " <tr class=\"boardRow\"> " +
-                            "   <th>"+results[i].uname+"</th> " +
+                            "   <th>"+results[i].username+"</th> " +
                             "   <th>"+results[i].score+"</th> " +
                             "</tr>";
     }
