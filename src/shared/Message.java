25c49ae9e0b2a68e902f2303fd4f3670bb9fb767
Luke St. Regis <stregis@usc.edu>
2018-04-08 21:33:44 -0700

    A working chat page, with an annoying 1/s server message

---
+++ src/shared/Message.java
@@ -0,0 +1,21 @@
+package shared;
+
+import java.io.Serializable;
+
+public class Message implements Serializable
+{
+    public String header;
+    public Object body;
+
+    public Message(String header, Object body)
+    {
+        this.header = header;
+        this.body = body;
+    }
+
+    public Message(String header)
+    {
+        this.header = header;
+        body = null;
+    }
+}
