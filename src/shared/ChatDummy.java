ef13a3f4015d89261aa4716dfb0a9fac317dfc92
Luke St. Regis <stregis@usc.edu>
2018-04-11 19:16:40 -0700

    ChatDummy now continuously attempts to connect to the websocket server, so the startup order of the backend and servlets doesn't matter

--- src/shared/ChatDummy.java
+++ src/shared/ChatDummy.java
@@ -1,15 +1,11 @@
 package shared;
 
 import org.apache.tomcat.websocket.WsContainerProvider;
-import shared.Connection;
-import shared.Message;
 
 import java.io.IOException;
 import java.net.Socket;
 import java.net.URI;
 import javax.websocket.ClientEndpoint;
-import javax.websocket.ContainerProvider;
-import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.WebSocketContainer;
@@ -20,16 +16,20 @@ public class ChatDummy
 
     public ChatDummy()
     {
-        try
+        Boolean connected = false;
+        while(!connected)
         {
-            WebSocketContainer container= WsContainerProvider.getWebSocketContainer();
-            String uri = "ws://localhost:8080/GeoQ/Chat";
-            container.connectToServer(this, new URI(uri));
-
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
+            try
+            {
+                WebSocketContainer container = WsContainerProvider.getWebSocketContainer();
+                String uri = "ws://localhost:8080/GeoQ/Chat";
+                container.connectToServer(this, new URI(uri));
+                connected = true;
+            }
+            catch(Exception e)
+            {
+                //Do nothing
+            }
         }
     }
 
