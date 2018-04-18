464146cdefec5f7dc1c59e6e3128c58b6ffce6cc
Luke St. Regis <stregis@usc.edu>
2018-04-18 10:59:53 -0700

    tweak the lobby so users get pulled in if game start time changes

--- src/backend/GameBackend.java
+++ src/backend/GameBackend.java
@@ -52,7 +52,7 @@ public class GameBackend
                             {
                                 try
                                 {
-                                    Thread.sleep(500);
+                                    Thread.sleep(100);
                                 }
                                 catch(InterruptedException e)
                                 {
@@ -137,8 +137,23 @@ public class GameBackend
                                 }
                             }
                         }
-                        c.close();
                     }
+                    else if(m.header.equals("lobbyPull"))
+                    {
+                        while(LocalDateTime.now().isBefore(state.settings.startTime))
+                        {
+                            try
+                            {
+                                Thread.sleep(100);
+                            }
+                            catch(InterruptedException e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }
+                        c.send("go");
+                    }
+                    c.close();
                 });
                 t.start();
             }
