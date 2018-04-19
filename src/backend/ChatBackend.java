320b04b5f63c263ef9296dc9c303a135ab580e4a
Nathan Zhang <nathanzh@usc.edu>
2018-04-18 17:04:47 -0700

    spurious leaderboard user fix

--- src/backend/ChatBackend.java
+++ src/backend/ChatBackend.java
@@ -38,7 +38,7 @@ public class ChatBackend
                     long millis = System.currentTimeMillis();
                     while(true)
                     {
-                        if((System.currentTimeMillis()-millis)%10 == 0)
+                        if((System.currentTimeMillis()-millis) > 10000)
                         {
                             millis = System.currentTimeMillis();
                             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm:ssa");
